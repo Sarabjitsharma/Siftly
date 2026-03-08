c3445f2d53cfc1946d850ca51e98f18108d9d88c
SARABJIT SHARMA <sarabjitsharma88@gmail.com>
2026-03-08 19:09:48 +0530

    feat: lazy load embedding model

--- Backend/src/embeddings/embeddings.py
+++ Backend/src/embeddings/embeddings.py
@@ -1,21 +1,34 @@
 from sentence_transformers import SentenceTransformer
 
-embedding_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+embedding_model = None
 
 
-def embed_query(query:str):
-    query = query
-    embedding = embedding_model.encode(
+def get_embedding_model():
+    global embedding_model
+
+    if embedding_model is None:
+        embedding_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+
+    return embedding_model
+
+
+def embed_query(query: str):
+    model = get_embedding_model()
+
+    embedding = model.encode(
         [query],
         normalize_embeddings=True
     )
+
     return embedding
 
 
 def embed_chunks(chunks):
+    model = get_embedding_model()
+
     texts = [chunk["text"] for chunk in chunks]
 
-    embeddings = embedding_model.encode(
+    embeddings = model.encode(
         texts,
         normalize_embeddings=True
     )
